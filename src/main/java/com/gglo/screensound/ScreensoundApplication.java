e2db2dd3997ac997e24085282c9fd947171578a0
Gustavo Oli <69ggustavoo69@gmail.com>
2024-05-29 10:49:11 -0300

    first commit

---
+++ src/main/java/com/gglo/screensound/ScreensoundApplication.java
@@ -0,0 +1,19 @@
+package com.gglo.screensound;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gglo.screensound.principal.Principal;
+
+@SpringBootApplication
+public class ScreensoundApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreensoundApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+	}
+}
